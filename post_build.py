9e66836721162456fa1299653573ed187c82a296
nicolas_g <nicolas.gemin@pm.me>
2026-01-10 09:09:32 -0500

    post_build

--- post_build.py
+++ post_build.py
@@ -1,15 +1,27 @@
 Import("env")
 
-import shutil
 import os
+import shutil
 
 def after_build(source, target, env):
     build_dir = env.subst("$BUILD_DIR")
-    src = os.path.join(build_dir, "firmware.bin")
-    dst = os.path.join(build_dir, "UM980-BLE.bin")
 
-    if os.path.exists(src):
-        shutil.copy(src, dst)
-        print("✔ Single BIN generated:", dst)
+    src = os.path.join(build_dir, "firmware.bin")
+    dst = os.path.join(build_dir, "UM980_BLE.bin")
+
+    if not os.path.exists(src):
+        print("✖ firmware.bin not found")
+        return
+
+    # Copy / rename
+    shutil.copy(src, dst)
+    print("✔ Single BIN generated:", dst)
+
+    # Remove original firmware.bin
+    try:
+        os.remove(src)
+        print("✔ firmware.bin removed")
+    except Exception as e:
+        print("✖ failed to remove firmware.bin:", e)
 
 env.AddPostAction("buildprog", after_build)
