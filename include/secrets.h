f239532bc585736a00fbf92b0853c9af7ea0c0bc
nicolas <nicolas.gemin@pm.me>
2026-02-01 20:06:50 -0500

    misc

--- include/secrets.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define STA_SSID_VALUE "64NDPVIWJCMG7RUZ9392"
-#define STA_PASS_VALUE "azerty1234"
-#define STA_IP_VALUE IPAddress(172, 20, 10, 2)
-#define STA_GW_VALUE IPAddress(172, 20, 10, 1)
-#define STA_SUBNET_VALUE IPAddress(255, 255, 255, 240)
-#define STA_DNS_VALUE IPAddress(172, 20, 10, 1)
